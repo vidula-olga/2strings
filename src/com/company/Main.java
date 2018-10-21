1ab656630134f0f9487d8bd1058fe64dc343d1c6
Vidula <olia.sivor@gmail.com>
2018-10-21 20:30:12 +0300

    split to words without empty strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    private static String[] splitToWords(String value) {
-        return value.split(" ");
+    private static List<String> splitToWords(String value) {
+        String[] words=value.split("[ ,\\.!\\(\\)]");
+        ArrayList<String> result = new ArrayList<>();
+        for (String word:words) {
+            if (! word.isEmpty())
+                result.add(word);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         String string1 = "cтрочка пробел, запятая. точка! восклицательный(знак)скобка";
         String string2 = "пробел точка знак";
-        String[] words1=splitToWords(string1);
-        String[] words2=splitToWords(string2);
+        List<String> words1=splitToWords(string1);
+        List<String> words2=splitToWords(string2);
 
         for (String word:words1) {
             System.out.println(word);
