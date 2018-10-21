58b7cf0633f13f57d16d14b6cd278a98a7141f84
Vidula <olia.sivor@gmail.com>
2018-10-21 20:19:48 +0300

    split to words

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
 public class Main {
+    private static String[] splitToWords(String value) {
+        return value.split(" ");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        String string1 = "cтрочка пробел, запятая. точка! восклицательный(знак)скобка";
+        String string2 = "пробел точка знак";
+        String[] words1=splitToWords(string1);
+        String[] words2=splitToWords(string2);
+
+        for (String word:words1) {
+            System.out.println(word);
+        }
+
+
     }
 }
